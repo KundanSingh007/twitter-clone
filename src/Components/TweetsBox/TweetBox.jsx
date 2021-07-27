abfcdf35b19a59e73ce63419611c2795df78fbdc
Kundan Singh <35429799+KundanSingh007@users.noreply.github.com>
2021-07-28 00:06:49 +0530

    tweetboxjsxand css

--- src/Components/TweetsBox/TweetBox.jsx
+++ src/Components/TweetsBox/TweetBox.jsx
@@ -9,7 +9,6 @@ function TweetBox() {
         <div className="tweetBox__input">
           <Avatar src="https://avatars.githubusercontent.com/u/35429799?v=4" />
           <input placeholder="whats going guys" type="text" />
-          <input placeholder="Enter image url" type="text" />
         </div>
         <Button>Tweet</Button>
       </form>
